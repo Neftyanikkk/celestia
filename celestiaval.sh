40ee08f2895f401ff1d8e8c0af15b1ee17373881
Roman <96354836+Zhoas@users.noreply.github.com>
2022-03-18 14:45:06 +0400

    Update celestiaval.sh

--- celestiaval.sh
+++ celestiaval.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 1
+# set your names
+echo -e '\n\e[42mYour node name:'
+read CELESTIA_NODENAME
+echo -e '\n\e[42mYour wallet name:'
+read CELESTIA_WALLET
+
+
 sudo apt update && sudo apt upgrade -y
 
-# Insall packages
+
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu -y
 
-# Install GO 1.17.2
 cd $HOME
 ver="1.17.2"
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
@@ -14,9 +21,6 @@ rm "go$ver.linux-amd64.tar.gz"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 go version
-
-# OUTPUT 
-# go version go1.17.2 linux/amd64
 
 cd $HOME
 rm -rf celestia-app
@@ -29,8 +33,6 @@ cd $HOME
 rm -rf networks
 git clone https://github.com/celestiaorg/networks.git
 
-CELESTIA_NODENAME="SERGOD"
-CELESTIA_WALLET="SERGOD"
 
 CELESTIA_CHAIN="devnet-2"  # do not change
 
@@ -40,33 +42,22 @@ echo 'export CELESTIA_NODENAME='${CELESTIA_NODENAME} >> $HOME/.bash_profile
 echo 'export CELESTIA_WALLET='${CELESTIA_WALLET} >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-# do init
 celestia-appd init $CELESTIA_NODENAME --chain-id $CELESTIA_CHAIN
-
-# copy devnet-2 genesis
 cp $HOME/networks/devnet-2/genesis.json $HOME/.celestia-app/config/
 
-# update seeds and peers
 SEEDS="74c0c793db07edd9b9ec17b076cea1a02dca511f@46.101.28.34:26656"
 PEERS="34d4bfec8998a8fac6393a14c5ae151cf6a5762f@194.163.191.41:26656"
 
 sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.celestia-app/config/config.toml
 
-# add external
+
 external_address=$(wget -qO- eth0.me)
 sed -i.bak -e "s/^external_address = \"\"/external_address = \"$external_address:26656\"/" $HOME/.celestia-app/config/config.toml
-
-# open rpc
 sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' $HOME/.celestia-app/config/config.toml
-
-# set proper defaults
 sed -i 's/timeout_commit = "5s"/timeout_commit = "15s"/g' $HOME/.celestia-app/config/config.toml
 sed -i 's/index_all_keys = false/index_all_keys = true/g' $HOME/.celestia-app/config/config.toml
-
-# open rest api
 sed -i '/\[api\]/{:a;n;/enabled/s/false/true/;Ta};/\[api\]/{:a;n;/enable/s/false/true/;Ta;}' $HOME/.celestia-app/config/app.toml
 
-# config pruning
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="5000"
@@ -77,29 +68,31 @@ sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_rec
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.celestia-app/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.celestia-app/config/app.toml
 
-# reset state
 celestia-appd unsafe-reset-all
 
-# OUTPUT
-# 12:04AM INF Removed all blockchain history dir=/root/.celestia-app/data
-# 12:04AM INF Reset private validator file to genesis state keyFile=/root/.celestia-app/config/priv_validator_key.json stateFile=/root/.celestia-app/data/priv_validator_state.json
-
-# add address book
 wget -O $HOME/.celestia-app/config/addrbook.json "https://raw.githubusercontent.com/maxzonder/celestia/main/addrbook.json"
 
 celestia-appd config chain-id $CELESTIA_CHAIN
 celestia-appd config keyring-backend test
-sudo tee /etc/systemd/system/celestia-appd.service > /dev/null <<EOF
+
+
+ExecStart=$HOME/go/bin/celestia
+
+sudo tee <<EOF >/dev/null /etc/systemd/system/celestia-appd.service
 [Unit]
-  Description=celestia-appd Cosmos daemon
-  After=network-online.target
+Description=celestia-appd Cosmos daemon
+After=network-online.target
+
 [Service]
-  User=$USER
-  ExecStart=$(which celestia-appd) start
-  Restart=on-failure
-  RestartSec=3
-  LimitNOFILE=4096
+User=$USER
+ExecStart=$HOME/go/bin/celestia-appd start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=4096
+
 [Install]
-  WantedBy=multi-user.target
+WantedBy=multi-user.target
 EOF
-celestia-appd keys add $CELESTIA_WALLET
+sudo systemctl enable celestia-appd
+sudo systemctl daemon-reload
+sudo systemctl restart celestia-appd && journalctl -u celestia-appd.service -f
